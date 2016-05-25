172ba0ff11fea0824f9ac7b59cc77efe0916172d
alexqin <alexqin@intv.com.cn>
2016-05-25 09:33:08 +0800

    update

--- js/intvvideo.js
+++ js/intvvideo.js
@@ -7,261 +7,268 @@ var intvVideo = (function(){
     var debug = false;
     // remove the base theme style
     window.VIDEOJS_NO_BASE_THEME = false;
-    var myFakePin, myActPin, duration, myControlPanel, myPin, myCurrentTimeTip, myProductList, myProductInfo, lastCurrentTime, lastTimestamp, lastMoment, lastMoment2, addMode, addListCount, addList, currentActiveListId, isDragging, startLeft;
+    var myFakePin, myActPin, duration, videoHeight, videoWidth, myControlPanel, myPin, myCurrentTimeTip, myProductList, myProductInfo, lastCurrentTime, lastTimestamp, lastMoment, lastMoment2, addMode, addListCount, currentActiveListId, isDragging, startLeft, myPlayer;
 
-    var secondsToHms, hmsToSeconds, addItem, updateList, removeFromList, drawRect, updateInput, getMaxId, formatTimeTip, log, updateProductList, getListItemById, triggerKeyEvent, getAddList;
+    addList = [];
 
-    // init the video player instance
-    var myPlayer = videojs('my-video', {
-        bigPlayButton: true
-    }, function () {
-        player = this,
-        myControlPanel = $('.control-panel'),
-        myPin = myControlPanel.find('span.current-time-pin'),
-        myCurrentTimeTip = $('.time-tip'),
-        myProductList = $('#product-list > tbody'),
-        myProductInfo = $('#product-info')
+    var secondsToHms, hmsToSeconds, addItem, updateList, removeFromList, drawRect, updateInput, getMaxId, formatTimeTip, log, updateProductList, getListItemById, triggerKeyEvent, getAddList, init;
 
-        lastCurrentTime = 0,
-        lastTimestamp = 0,
-        lastMoment = 0,
-        lastMoment2 = 0,
+    init = function() {
+        // init the video player instance
+        myPlayer = videojs('my-video', {
+            bigPlayButton: true
+        }, function () {
+            player = this,
+            myControlPanel = $('.control-panel'),
+            myPin = myControlPanel.find('span.current-time-pin'),
+            myCurrentTimeTip = $('.time-tip'),
+            myProductList = $('#product-list > tbody'),
+            myProductInfo = $('#product-info')
 
-        addMode = false,
-        addListCount = 0,
-        addList = [],
-        currentActiveListId = 0,
-        isDragging = false,
-        startLeft = 0;
+            lastCurrentTime = 0,
+            lastTimestamp = 0,
+            lastMoment = 0,
+            lastMoment2 = 0,
 
-        player.on('loadedmetadata', function () {
-            duration = player.duration();
-            myCurrentTimeTip.html(formatTimeTip(0, duration));
-        });
-
-        player.on('timeupdate', function () {
-
-            var currentTime = this.currentTime();
-            // var duration = this.duration();
-
-            myPin.css({
-                'left': (currentTime / duration * 100) + '%',
-            })
-
-            myCurrentTimeTip.html(formatTimeTip(currentTime, duration));
-
-            lastCurrentTime = currentTime;
-
-        });
-
-        myControlPanel.on('mousedown', function (e) {
-
-            if (!addMode) return
-
-            isDragging = true;
-
-            startLeft = parseInt((e.clientX - myControlPanel.position().left) / myControlPanel.width() * duration);
-
-
-            if ($.find('#act-pin-' + currentActiveListId).length) {
-                myActPin = $('#act-pin-' + currentActiveListId);
-            } else {
-                $('span.act-pin.active').removeClass('active');
-                myActPin = $('<span id="act-pin-' + currentActiveListId + '" class="act-pin active"></span>').appendTo(myControlPanel);
-            }
-
-        });
-
-        myControlPanel.on('mouseup', function (e) {
-
-            if (!addMode) return
-
-            myActPin.text(currentActiveListId);
+            addMode = false,
+            addListCount = 0,
+            currentActiveListId = 0,
+            isDragging = false,
             startLeft = 0;
-            isDragging = false;
 
-        });
+            player.on('loadedmetadata', function () {
+                duration = player.duration();
+                videoHeight = player.videoHeight();
+                videoWidth = player.videoWidth();
+                myCurrentTimeTip.html(formatTimeTip(0, duration));
 
-        myControlPanel.on('mouseenter', function (e) {
 
-            if (!player.paused()) return
+                player.on('timeupdate', function () {
 
-            if (!addMode) {
-                myFakePin = $('<span class="fake-pin"></span>').appendTo(myControlPanel);
-            }
+                    var currentTime = this.currentTime();
+                    // var duration = this.duration();
 
-        });
+                    myPin.css({
+                        'left': (currentTime / duration * 100) + '%',
+                    })
 
-        myControlPanel.on('mousemove', function (e) {
+                    myCurrentTimeTip.html(formatTimeTip(currentTime, duration));
 
-            if (!player.paused() || (e.timeStamp - lastTimestamp < 50)) {
-                lastTimestamp = e.timeStamp;
-                return false;
-            }
+                    lastCurrentTime = currentTime;
 
-            var delta = e.clientX - myControlPanel.position().left;
-            var currentMoment = parseInt(delta / myControlPanel.width() * duration);
+                });
 
-            if (addMode) {
-                debug && console.log('add mode is on [mouse move]');
-                if (isDragging) {
-                    debug && console.log('startleft : ' + startLeft);
+                myControlPanel.on('mousedown', function (e) {
 
-                    debug && console.log('addMode currentMoment : ' + currentMoment);
-                    debug && console.log('addMode delta : ' + delta);
+                    if (!addMode) return
 
-                    if (lastMoment2 === currentMoment) {
+                    isDragging = true;
+
+                    startLeft = parseInt((e.clientX - myControlPanel.offset().left) / myControlPanel.width() * duration);
+
+
+                    if ($.find('#act-pin-' + currentActiveListId).length) {
+                        myActPin = $('#act-pin-' + currentActiveListId);
+                    } else {
+                        $('span.act-pin.active').removeClass('active');
+                        myActPin = $('<span id="act-pin-' + currentActiveListId + '" class="act-pin active"></span>').appendTo(myControlPanel);
+                    }
+
+                });
+
+                myControlPanel.on('mouseup', function (e) {
+
+                    if (!addMode) return
+
+                    myActPin.text(currentActiveListId);
+                    startLeft = 0;
+                    isDragging = false;
+
+                });
+
+                myControlPanel.on('mouseenter', function (e) {
+
+                    if (!player.paused()) return
+
+                    if (!addMode) {
+                        myFakePin = $('<span class="fake-pin"></span>').appendTo(myControlPanel);
+                    }
+
+                });
+
+                myControlPanel.on('mousemove', function (e) {
+
+                    if (!player.paused() || (e.timeStamp - lastTimestamp < 50)) {
+                        lastTimestamp = e.timeStamp;
                         return false;
                     }
 
-                    lastMoment2 = currentMoment;
+                    var delta = e.clientX - myControlPanel.offset().left;
+                    var currentMoment = parseInt(delta / myControlPanel.width() * duration);
 
-                    drawRect(myActPin, currentActiveListId, startLeft, currentMoment);
+                    if (addMode) {
+                        debug && console.log('add mode is on [mouse move]');
+                        if (isDragging) {
+                            debug && console.log('startleft : ' + startLeft);
 
-                    myProductInfo.find('.start input.time').val(secondsToHms(startLeft > currentMoment ? currentMoment : startLeft));
-                    myProductInfo.find('.end input.time').val(secondsToHms(parseInt(currentMoment > startLeft ? currentMoment : startLeft)));
+                            debug && console.log('addMode currentMoment : ' + currentMoment);
+                            debug && console.log('addMode delta : ' + delta);
 
-                    myCurrentTimeTip.html(formatTimeTip(currentMoment, duration));
+                            if (lastMoment2 === currentMoment) {
+                                return false;
+                            }
 
-                }
-            } else {
+                            lastMoment2 = currentMoment;
 
-                debug && console.log('lastMoment: ' + lastMoment);
-                debug && console.log('currentMoment: ' + currentMoment);
+                            drawRect(myActPin, currentActiveListId, startLeft, currentMoment);
 
-                if (currentMoment === lastMoment) {
-                    return false;
-                }
+                            myProductInfo.find('.start input.time').val(secondsToHms(startLeft > currentMoment ? currentMoment : startLeft));
+                            myProductInfo.find('.end input.time').val(secondsToHms(parseInt(currentMoment > startLeft ? currentMoment : startLeft)));
 
-                lastMoment = currentMoment;
-                var leftStyle = (currentMoment / duration) * 100;
-                myFakePin.css('left', leftStyle + '%');
-                myCurrentTimeTip.html(formatTimeTip(currentMoment, duration));
+                            myCurrentTimeTip.html(formatTimeTip(currentMoment, duration));
 
-            }
+                        }
+                    } else {
+
+                        debug && console.log('lastMoment: ' + lastMoment);
+                        debug && console.log('currentMoment: ' + currentMoment);
+
+                        if (currentMoment === lastMoment) {
+                            return false;
+                        }
+
+                        lastMoment = currentMoment;
+                        var leftStyle = (currentMoment / duration) * 100;
+                        myFakePin.css('left', leftStyle + '%');
+                        myCurrentTimeTip.html(formatTimeTip(currentMoment, duration));
+
+                    }
+                });
+
+                myControlPanel.on('mouseleave', function (e) {
+
+                    if (!player.paused()) return
+
+                    if (addMode) {
+                        debug && console.log('add mode is on [mouse leave]');
+                    } else {
+                        myFakePin.remove();
+                        var currentTime = player.currentTime();
+                        myCurrentTimeTip.html(formatTimeTip(currentTime, duration));
+                    }
+
+                });
+
+                myControlPanel.on('click', function (e) {
+
+                    if (!player.paused()) return
+
+                    if (!addMode) {
+                        myPin.attr('style', myFakePin.attr('style'));
+                        player.currentTime(myPin[0].style.left.replace('%', '') * duration / 100);
+                    }
+
+                });
+
+                $(document.body).on('keyup', function (e) {
+
+                    if (e.shiftKey && e.keyCode === 27) { // SHIFT + ESC
+                        $('.help-layer').toggle();
+                    } else if (e.keyCode === 27) { // if 'ESC' pressed reset the pin to 0
+                        myPin.css('left', '0');
+                        player.currentTime(0);
+                    }
+
+                });
+
+                // 添加新商品
+                myProductInfo.on('click', 'button.add', function() {
+
+                    var id = getMaxId() + 1;
+                    currentActiveListId = id;
+                    addItem(id);
+                    addMode = true;
+                });
+
+                // 保存当前编辑商品
+                myProductInfo.on('click', 'button.save', function(e) {
+                    var $node = $(this);
+                    updateList($node.parent());
+                });
+
+                // 按键调整输入标签的值
+                myProductInfo.on('keydown', 'input.time', function (e) {
+
+                    var $inputNode = $(this),
+                        count = hmsToSeconds($inputNode.val()),
+                        weight = e.shiftKey ? 10 : 1;
+
+                    if (e.keyCode === 38) { //up
+                        count += weight;
+                        count = count > parseInt(duration) ? parseInt(duration) : count;
+                        $inputNode.val(secondsToHms(count));
+                    } else if (e.keyCode === 40) { // down
+                        count -= weight;
+                        count = (count > 0) ? count : 0; // avoid negtive number
+                        $inputNode.val(secondsToHms(count));
+                    } else {
+                        $inputNode.trigger('input');
+                    }
+
+                    updateInput();
+
+                });
+
+                // 失去焦点的时候调整输入标签的值
+                myProductInfo.on('blur', 'input.time', function () {
+
+                    var $inputNode = $(this),
+                        val = $(this).val();
+
+                    val = hmsToSeconds(val)
+                    val = Number.isNaN(val) ? 0 : val;
+
+                    $inputNode.val(secondsToHms(val));
+                    updateInput()
+
+                });
+
+
+                myProductInfo.on('click', '.input-time > span', function(e) {
+                    var isAdd = e.target.className === 'plus';
+                    var $nodeInput = $(this).parent().find('input');
+                    triggerKeyEvent($nodeInput, isAdd);
+                });
+
+
+                myProductInfo.on('click', 'button.back', function() {
+                    addMode = false;
+                });
+
+                // 更新当前添加的商品列表项
+                $('#product-list').on('click', 'tbody > tr button', function(e) {
+
+                    var id = $(e.target).data('activeId');
+
+                    if(e.target.className === 'delete') {
+                        removeFromList(id);
+                    } else {
+                        currentActiveListId = id;
+                        addMode = true;
+                        $('span.act-pin.active').removeClass('active');
+                        $('#act-pin-' + currentActiveListId).addClass('active');
+                        var item = getListItemById(currentActiveListId);
+                        myProductInfo.find('.start input.time').val(secondsToHms(item.startTime));
+                        myProductInfo.find('.end input.time').val(secondsToHms(item.endTime));
+                    }
+
+
+                });
+
+                updateProductList(true);
+            });
         });
-
-        myControlPanel.on('mouseleave', function (e) {
-
-            if (!player.paused()) return
-
-            if (addMode) {
-                debug && console.log('add mode is on [mouse leave]');
-            } else {
-                myFakePin.remove();
-                var currentTime = player.currentTime();
-                myCurrentTimeTip.html(formatTimeTip(currentTime, duration));
-            }
-
-        });
-
-        myControlPanel.on('click', function (e) {
-
-            if (!player.paused()) return
-
-            if (!addMode) {
-                myPin.attr('style', myFakePin.attr('style'));
-                player.currentTime(myPin[0].style.left.replace('%', '') * duration / 100);
-            }
-
-        });
-
-        $(document.body).on('keyup', function (e) {
-
-            if (e.shiftKey && e.keyCode === 27) { // SHIFT + ESC
-                $('.help-layer').toggle();
-            } else if (e.keyCode === 27) { // if 'ESC' pressed reset the pin to 0
-                myPin.css('left', '0');
-                player.currentTime(0);
-            }
-
-        });
-
-        // 添加新商品
-        myProductInfo.on('click', 'button.add', function() {
-
-            var id = getMaxId() + 1;
-            currentActiveListId = id;
-            addItem(id);
-            addMode = true;
-        });
-
-        // 保存当前编辑商品
-        myProductInfo.on('click', 'button.save', function(e) {
-            var $node = $(this);
-            updateList($node.parent());
-        });
-
-        // 按键调整输入标签的值
-        myProductInfo.on('keydown', 'input.time', function (e) {
-
-            var $inputNode = $(this),
-                count = hmsToSeconds($inputNode.val()),
-                weight = e.shiftKey ? 10 : 1;
-
-            if (e.keyCode === 38) { //up
-                count += weight;
-                count = count > parseInt(duration) ? parseInt(duration) : count;
-                $inputNode.val(secondsToHms(count));
-            } else if (e.keyCode === 40) { // down
-                count -= weight;
-                count = (count > 0) ? count : 0; // avoid negtive number
-                $inputNode.val(secondsToHms(count));
-            } else {
-                $inputNode.trigger('input');
-            }
-
-            updateInput();
-
-        });
-
-        // 失去焦点的时候调整输入标签的值
-        myProductInfo.on('blur', 'input.time', function () {
-
-            var $inputNode = $(this),
-                val = $(this).val();
-
-            val = hmsToSeconds(val)
-            val = Number.isNaN(val) ? 0 : val;
-
-            $inputNode.val(secondsToHms(val));
-            updateInput()
-
-        });
-
-
-        myProductInfo.on('click', '.input-time > span', function(e) {
-            var isAdd = e.target.className === 'plus';
-            var $nodeInput = $(this).parent().find('input');
-            triggerKeyEvent($nodeInput, isAdd);
-        });
-
-
-        myProductInfo.on('click', 'button.back', function() {
-            addMode = false;
-        });
-
-        // 更新当前添加的商品列表项
-        $('#product-list').on('click', 'tbody > tr button', function(e) {
-
-            var id = $(e.target).data('activeId');
-
-            if(e.target.className === 'delete') {
-                removeFromList(id);
-            } else {
-                currentActiveListId = id;
-                addMode = true;
-                $('span.act-pin.active').removeClass('active');
-                $('#act-pin-' + currentActiveListId).addClass('active');
-                var item = getListItemById(currentActiveListId);
-                myProductInfo.find('.start input.time').val(secondsToHms(item.startTime));
-                myProductInfo.find('.end input.time').val(secondsToHms(item.endTime));
-            }
-
-
-        });
-    });
-
+    }
     secondsToHms = function(d) {
 
         d = Number(d);
@@ -357,8 +364,6 @@ var intvVideo = (function(){
         }
         var $node = dom || $('#act-pin-' + id);
 
-        console.log($node)
-
         // select from left to right [->] || right to left [<-]
         leftStyle = (end > start) ? (start / duration * 100) : (end / duration * 100)
 
@@ -446,9 +451,20 @@ var intvVideo = (function(){
         addList = list;
     }
 
+
+    getVideoInfo = function() {
+        return {
+            height: videoHeight,
+            width: videoWidth
+        }
+    }
     return {
+        init: init,
         getGoods: getAddList,
         setGoods: setAddList,
-        updateProductList: updateProductList
+        updateProductList: updateProductList,
+        getVideoInfo: getVideoInfo 
     }
 })();
+
+intvVideo.init()
